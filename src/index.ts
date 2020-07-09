c5a7f7338016d96b0d3a6b9f5da6b29de1dc60c1
Christian Alfoni <christianalfoni@gmail.com>
2020-07-09 17:30:48 +0200

    fix(typing): use ComponentType as returned type
    though a function is a valid component type, things like defaultProps etc. is not typed

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,7 @@ export { Box } from './Box';
 export type PolymorphicComponentProps<E extends React.ElementType, P> = P &
   BoxProps<E>;
 
-export type PolymorphicComponent<P, D extends React.ElementType = 'div'> = <
-  E extends React.ElementType = D
->(
-  props: PolymorphicComponentProps<E, P>,
-) => JSX.Element;
+export type PolymorphicComponent<
+  P,
+  D extends React.ElementType = 'div'
+> = React.ComponentType<PolymorphicComponentProps<D, P>>;
