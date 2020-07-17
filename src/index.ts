2ec5d48b539abb075f5846408e4c5de252912d8c
Kristóf Poduszló <kripod@protonmail.com>
2020-07-17 18:58:47 +0200

    revert: "fix(typing): use ComponentType as returned type"
    This reverts commit c5a7f7338016d96b0d3a6b9f5da6b29de1dc60c1.

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ export { Box } from "./Box";
 export type PolymorphicComponentProps<E extends React.ElementType, P> = P &
 	BoxProps<E>;
 
-export type PolymorphicComponent<
-	P,
-	D extends React.ElementType = "div"
-> = React.ComponentType<PolymorphicComponentProps<D, P>>;
+export type PolymorphicComponent<P, D extends React.ElementType = "div"> = <
+	E extends React.ElementType = D
+>(
+	props: PolymorphicComponentProps<E, P>,
+) => JSX.Element;
