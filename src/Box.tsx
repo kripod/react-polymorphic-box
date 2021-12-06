6628265e17fb1c43e3eece3f6bbcfd12c2943c68
Yoki Yu <yokiyuqian@gmail.com>
2021-12-06 12:35:14 +0800

    feat: implement correct ref type from element passed by as prop

--- src/Box.tsx
+++ src/Box.tsx
@@ -2,19 +2,19 @@ import * as React from "react";
 
 export type BoxOwnProps<E extends React.ElementType = React.ElementType> = {
 	as?: E;
+	ref?: React.ComponentProps<E>["ref"];
 };
 
-export type BoxProps<E extends React.ElementType> = BoxOwnProps<E> &
-	Omit<React.ComponentProps<E>, keyof BoxOwnProps>;
+export type BoxProps<
+	E extends React.ElementType = React.ElementType
+> = BoxOwnProps<E> & Omit<React.ComponentPropsWithoutRef<E>, never>;
 
 const defaultElement = "div";
 
 export const Box: <E extends React.ElementType = typeof defaultElement>(
 	props: BoxProps<E>,
-) => React.ReactElement | null = React.forwardRef(function Box(
-	props: BoxOwnProps,
-	ref: React.Ref<Element>,
-) {
-	const Element = props.as || defaultElement;
+) => React.ReactElement | null = React.forwardRef(function Box(props, ref) {
+	// eslint-disable-next-line react/prop-types
+	const Element = props.as ?? defaultElement;
 	return <Element ref={ref} {...props} as={undefined} />;
 });
