b16f0da0f5b63105bf8380b104db2391001843ba
Yoki Yu <yokiyuqian@gmail.com>
2021-12-06 14:25:41 +0800

    fix: ts types errors

--- src/Box.tsx
+++ src/Box.tsx
@@ -2,19 +2,20 @@ import * as React from "react";
 
 export type BoxOwnProps<E extends React.ElementType = React.ElementType> = {
 	as?: E;
-	ref?: React.ComponentProps<E>["ref"];
+	ref?: React.ComponentProps<E>;
 };
 
-export type BoxProps<
-	E extends React.ElementType = React.ElementType
-> = BoxOwnProps<E> & Omit<React.ComponentPropsWithoutRef<E>, never>;
+export type BoxProps<E extends React.ElementType> = BoxOwnProps<E> &
+	Omit<React.ComponentPropsWithoutRef<E>, keyof BoxOwnProps>;
 
 const defaultElement = "div";
 
 export const Box: <E extends React.ElementType = typeof defaultElement>(
 	props: BoxProps<E>,
-) => React.ReactElement | null = React.forwardRef(function Box(props, ref) {
-	// eslint-disable-next-line react/prop-types
-	const Element = props.as ?? defaultElement;
+) => React.ReactElement | null = React.forwardRef(function Box(
+	props: BoxOwnProps,
+	ref,
+) {
+	const Element = props.as || defaultElement;
 	return <Element ref={ref} {...props} as={undefined} />;
 });
